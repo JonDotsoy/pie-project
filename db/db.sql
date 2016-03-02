570623a7f4453d685aae2da50a8f011d89022d26
Jon Dotsoy <hi@jon.soy>
2016-03-01 23:00:21 -0300

    docker para mysql

--- db/db.sql
+++ db/db.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `actividad`
 --
 
+use pie;
+
 DROP TABLE IF EXISTS `actividad`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
