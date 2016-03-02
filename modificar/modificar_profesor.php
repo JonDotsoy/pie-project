5cecc7f2818d7b4c3b0eedab6e03346525bc15a9
Jon Dotsoy <hi@jon.soy>
2016-03-02 15:55:33 -0300

    Update tablerosorter

--- modificar/modificar_profesor.php
+++ modificar/modificar_profesor.php
@@ -13,7 +13,7 @@
 	<![endif]-->
 	<script src="../js/jquery-1.5.2.min.js" type="text/javascript"></script>
 	<script src="../js/hideshow.js" type="text/javascript"></script>
-	<script src="../js/jquery.tablesorter.min.js" type="text/javascript"></script>
+	<script src="../js/jquery.tablesorter.js" type="text/javascript"></script>
 	<script type="text/javascript" src="../js/jquery.equalHeight.js"></script>
 	<script type="text/javascript">
 	$(document).ready(function() 
